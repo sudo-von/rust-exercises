12059e5fdc63306d33d4d5ab48771d58ba70ea4b
VoN <sudo.von.contact@gmail.com>
2022-06-17 00:06:38 -0500

    feat: :sparkles: Added variables

---
+++ ultimate-rust-crash-course/variables-01/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let bunnies: i32 = 4;
+    println!("{}", bunnies);
+    let (bunny, carrot) = (8, 50);
+    println!("{} - {}", bunny, carrot);
+    let mut dogs: i32 = 4;
+    println!("{}", dogs);
+    dogs = 10;
+    println!("{}", dogs);
+    const CATS: i32 = 10;
+    println!("{}", CATS);
+}
