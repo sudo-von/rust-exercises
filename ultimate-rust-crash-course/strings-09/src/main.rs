d2528ad752b2ed0349cd9f61c3f3d79064964c93
VoN <sudo.von.contact@gmail.com>
2022-06-18 17:07:05 -0500

    feat: :sparkles: Added strings

---
+++ ultimate-rust-crash-course/strings-09/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    /* There are 6 types of strings in the std */
+    let msg = "Hello";
+    let msg = "abc".to_string();
+    /* Unicode can be represented by 1,2,3 or 4 bytes. */
+    let msg = String::from("abc");
+}
