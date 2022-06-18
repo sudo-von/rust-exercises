b46bdde2e7b28e7c757fc10848668a404594eab2
VoN <sudo.von.contact@gmail.com>
2022-06-18 16:33:13 -0500

    feat: :sparkles: Added scalar types

---
+++ ultimate-rust-crash-course/scalar-types-06/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    /*
+     * Unsigned Signed
+     * u8       i8
+     * u16      i16
+     * u32      i32
+     * u64      i64
+     * u128     i128
+     * usize    isize
+     * 
+    */
+
+    /*
+     * Integer literals
+     * Decimal 1_0_0_0_0_0_0
+     * Hex     0xdeadbeef
+     * Octal   0o77543211
+     * Binary  0b11_11_00_11
+     * Byte (u8 onlu) b'A'
+     * Underscores are ignored
+    */
+
+    /*
+     * f32
+     * f64
+    */
+
+    /* 
+     * booleans
+     * true
+     * false
+    */
+
+    /*
+     * char
+     * @
+     * {
+     * -
+     * 'A'
+     * 4 bytes (32 bits)
+    */
+}
