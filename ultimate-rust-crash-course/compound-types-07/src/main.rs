8c152f386b99f35829e902a302df57318e396344
VoN <sudo.von.contact@gmail.com>
2022-06-18 16:37:23 -0500

    feat: :sparkles: Added compound types

---
+++ ultimate-rust-crash-course/compound-types-07/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    /* Tuple */
+    let info: (u8, f64, f32) = (1, 1.0, 0.5); 
+    // let jets = info.0;
+    // let fuel = info.1;
+    // let ammo = info.2;
+    let (jets, fuel, ammod) = info;
+    /* Arrays. */
+    /* Value; How many. */
+    let buf = [0; 3];
+    let another_buf: [u8; 3] = [1,2,3];
+    let first_buf = buf[0];
+}
