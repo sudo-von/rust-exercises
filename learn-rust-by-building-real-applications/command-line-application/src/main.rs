a550dcb44305272416514ef7eb342d9e6ed46f81
VoN <sudo.von.contact@gmail.com>
2022-07-10 01:36:43 -0500

    feat: :sparkles: Changed variable to be mutable

--- learn-rust-by-building-real-applications/command-line-application/src/main.rs
+++ learn-rust-by-building-real-applications/command-line-application/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let weight: f32 = calculate_weight_on_mars(100.0);
-    println!("Weight on Mars: {} kg", weight);
+    let mut margs_weight: f32 = calculate_weight_on_mars(100.0);
+    margs_weight = margs_weight * 1000.0;
+    println!("Weight on Mars: {} kg", margs_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
