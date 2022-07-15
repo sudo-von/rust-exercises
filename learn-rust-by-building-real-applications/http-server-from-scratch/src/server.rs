216c630679788c01fda65574cf1abd5d65037134
VoN <sudo.von.contact@gmail.com>
2022-07-14 19:22:27 -0500

    feat: :sparkles: Added tcp listener

--- learn-rust-by-building-real-applications/http-server-from-scratch/src/server.rs
+++ learn-rust-by-building-real-applications/http-server-from-scratch/src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 pub struct Server {
     address: String,
 }
@@ -9,5 +11,6 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.address);
+        let listener = TcpListener::bind(&self.address);
     }
 }
