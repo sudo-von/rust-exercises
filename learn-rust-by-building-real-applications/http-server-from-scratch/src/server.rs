34563516a550f2dd15ceca08fb041cacd67029cb
VoN <sudo.von.contact@gmail.com>
2022-07-16 23:42:52 -0500

    feat: :sparkles: Added traits

--- learn-rust-by-building-real-applications/http-server-from-scratch/src/server.rs
+++ learn-rust-by-building-real-applications/http-server-from-scratch/src/server.rs
@@ -1,3 +1,5 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -21,7 +23,11 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse from: {}", e),
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
